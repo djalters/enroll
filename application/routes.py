c2da920aa23a52f5f163c3b662454d8ab0cf989a
djalters <walters.eye@gmail.com>
2020-03-30 09:07:26 +0100

    api endpoint pass data by index, see button on courses page (action=). data drawn globally, i.e. by database

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,24 @@
 from application import app
-from flask import render_template, request
+from flask import render_template, request, Response, json
+
+#moved to global space to test api
+#data source, this is one record, passed as props
+courseData = [
+    {
+        "courseID": "5887463",
+        "title": "my course 10453",
+        "description": "my desc",
+        "credits": "100",
+        "term": "1"
+    },
+    {
+        "courseID": "234324",
+        "title": "my new 3",
+        "description": "my other desc",
+        "credits": "400",
+        "term": "5"
+    }
+]
 
 #root directory. URL routes can be defined here. Root/index will call/render this function
 @app.route("/")
@@ -15,16 +34,6 @@ def login():
 @app.route("/courses/")
 @app.route("/courses/<term>")
 def courses(term="Fall 2019"):
-    #data source, this is one record, passed as props
-    courseData = [
-        {
-            "courseID": "5887463",
-            "title": "my course 10453",
-            "description": "my desc",
-            "credits": "100",
-            "term": "1"
-        }
-    ]
 
     return render_template("courses.html", courses=True, courseData=courseData, term=term)
 
@@ -32,9 +41,19 @@ def courses(term="Fall 2019"):
 def register():
     return render_template("index.html", register=True)
 
-@app.route("/enrollment")
+@app.route("/enrollment", methods=["GET", "POST"])
 def enrollment():
-    id = request.args.get('courseID')
-    title = request.args.get('title')
-    term  = request.args.get('term')
+    id = request.form.get('courseID')
+    title = request.form.get('title')
+    term  = request.form.get('term')
     return render_template("enrollment.html", enrollment=True, data = {"id":id, "title":title, "term":term})
+
+@app.route("/api/")
+@app.route("/api/<idx>")
+def api(idx=None):
+    if idx==None:
+        jdata = courseData
+    else:
+        jdata = courseData[int(idx)]
+
+    return Response(json.dumps(jdata), mimetype="application/json")
